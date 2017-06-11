09513f521bd8a3e5841cb915c4159869815ab72d
Marcel <marcel.sawicki@hotmail.com>
2017-06-11 17:41:34 +0200

    Kalkulator

---
+++ Kalkulator/Kalkulator.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Kalkulator.aspx.cs" Inherits="Kalkulator.Kalkulator" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+
+<br />
+<br />
+<asp:TextBox ID="TextBox1" runat="server" Width="140px"></asp:TextBox>
+<asp:TextBox ID="TextBox2" runat="server" Width="141px"></asp:TextBox>
+<br />
+<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Dodaj" />
+<br />
+<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Odejmij" />
+<br />
+<asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Mnóż" />
+<br />
+<asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Dziel" />
+<br />
+<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+
+</asp:Content>
---
+++ Kalkulator/Kalkulator.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kalkulator
+{
+    public partial class Kalkulator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*public interface ILiczydlo
+        {
+            int dodaj(int l1, int l2);
+            int odejmij(int l1, int l2);
+            int mnoz(int l1, int l2);
+            float dziel(float l1, float l2);
+        } */
+
+        public class kalkulator//: ILiczydlo
+        {
+
+            public static int dodaj(int l1, int l2)
+            {return l1 + l2;}
+
+            public static int odejmij(int l1, int l2)
+            { return l1 - l2; }
+
+            public static int mnoz(int l1, int l2)
+            { return l1 * l2; }
+
+            public static float dziel(float l1, float l2)
+            {
+                try
+                {
+                    float wynik = l1 / l2;
+                    if (l2 == 0)
+                        throw new DivideByZeroException();
+                    return wynik;
+                }
+                catch(DivideByZeroException) {
+                    return 0;
+                }
+
+                
+            }
+
+           public kalkulator() {
+               //konstruktor klasy
+           }
+            }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int wynik = kalkulator.dodaj(
+            Int32.Parse(TextBox1.Text), Int32.Parse(TextBox2.Text));
+            Label1.Text = "Wynik " + wynik;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            
+            int wynik = kalkulator.odejmij(
+            Int32.Parse(TextBox1.Text), Int32.Parse(TextBox2.Text));
+            Label1.Text = "Wynik " + wynik;
+        }
+
+        public class DzielZero : ApplicationException
+        {
+            public int errorCode;
+            public DzielZero()
+            { errorCode = 10; }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int wynik = kalkulator.mnoz(
+            Int32.Parse(TextBox1.Text), Int32.Parse(TextBox2.Text));
+            Label1.Text = "Wynik " + wynik;
+        }
+
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float wynik = kalkulator.dziel(
+                Int32.Parse(TextBox1.Text), Int32.Parse(TextBox2.Text));
+                if (wynik == 0) throw new DzielZero();
+                Label1.Text = "Wynik " + wynik;
+            }
+
+            catch (DzielZero)
+            {
+                Label1.Text = "Wynik " + "Dzielenie przez zero!";
+            }
+            
+            //catch(DivideByZeroException) { 
+            //    Label1.Text = "Wynik " + "Dzielenie przez zero!";
+            //}
+            
+            catch (FormatException)
+            {
+                Label1.Text = "Wynik " + "Podaj liczbe!";
+            }
+            catch (OverflowException)
+            {
+                Label1.Text = "Wynik " + "Przekroczono!";
+            }
+        }
+    }
+}
