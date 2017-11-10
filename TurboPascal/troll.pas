54a743c096f3f9c69e7ec95ab8becf9c8cb9d03b
msawicki <msawicki@it-dev.pl>
2017-11-10 18:24:45 +0100

    Pascal

---
+++ TurboPascal/troll.pas
@@ -0,0 +1,83 @@
+program rpg;
+uses
+	crt;
+const 
+	ile_zdarzen=14;
+type
+	r_zdarzenia = { wrog, nieznajomy, przedmiot, nic };
+	r_nieznajomego = { zlodziej, dobry_d, zly_d, czarodziej };
+	r_przedmiotu = { miecz, zbroje, eliksir, mieso, zloto, trucizna };
+
+zdarzenie = record
+				opis: string;
+				prawdo: integer;
+				case rodzaj: r_zdarzenia of
+								wrog: { sila: integer };
+								przedmiot: { rzecz: r_przedmiotu };
+								nieznajomy: { kto: r_nieznajomego };
+				end;
+const 
+	hero: bohater = { wytrzymalosc: 4; sila: 4; bagaz: []; zloto: 0};
+	swiat: array[1..ile_zdarzen] of zdarzenie = (
+	(opis:'nic'; prawdo:40; rodzaj:nic),
+	(opis:'oblesny gnom'; prawdo:10; rodzaj:wrog; sila:2),
+	(opis:'wielki smok'; prawdo:5; rodzaj:wrog; sila:7)
+	);
+
+var
+	ile: integer;
+	klawisz: char;
+
+procedure opisz_bohatera;
+var	przed: r_przedmiotu;
+begin
+	clrscr;
+	writeln('wytrzymalosc: ', hero.wytrzymalosc, 'sila: ', hero.sila, 'zloto: ', hero.zloto);
+	write('bagaz: ');
+	for przed:=miecz to trucizna do
+		if przed in hero.bagaz then
+			write(nazwa_p[przed],' ');
+	writeln;
+	writeln('*******************************');
+end;
+
+procedure spotkaj_wroga( kto: zdarzenie );
+begin
+end;
+
+procedure spotkaj_nieznajomego(ktos: zdarzenie);
+begin
+end;
+
+procedure znajdz_przedmiot(co: zdarzenie);
+var	
+	los: integer;
+begin
+	writeln('znalazles ', co.opis);
+	los:=random(20)+20;
+	
+	case co.rzecz of
+		miecz: hero.bagaz:= hero.bagaz[miecz];
+		zbroja: hero.bagaz:= hero.bagaz[zbroja];
+		eliksir: hero.wytrzymalosc:=hero.wytrzymalosc+1;
+		mieso: hero.sila:=hero.sila+1;
+		zloto: hero.zloto:=hero.zloto+1;
+		trucizna: 
+			begin
+				hero.wytrzymalosc:= hero.wytrzymalosc div 2;
+				hero.sila:= hero.sila div 2;
+			end;
+	end;
+end;
+
+begin
+	randomize;
+	opisz_bohatera;
+	while hero.wytrzymalosc>0 do
+		begin
+		end;
+	writeln('Koniec gry');
+	if hero.zloto>1000 then
+		writeln('wygrales');
+	klawisz:=readkey;
+end.
