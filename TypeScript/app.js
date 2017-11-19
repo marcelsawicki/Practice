ba68efff8acf0000302f6a2c9e874c2a5bb7a1ac
msawicki <msawicki@it-dev.pl>
2017-11-19 18:30:07 +0100

    Classes.

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -36,11 +36,90 @@ var Hafn = (function () {
     };
     return Hafn;
 }());
+var r_zdarzenia = ['wrog', 'nieznajomy', 'przedmiot', 'nic'];
+var r_nieznajomego = ['zlodziej', 'dobry_d', 'zly_d', 'czarodziej'];
+var r_przedmiotu = ['miecz', 'zbroja', 'eliksir', 'mieso', 'zloto', 'trucizna'];
+var OpisSwiata = (function () {
+    function OpisSwiata() {
+    }
+    return OpisSwiata;
+}());
+var swiat = [
+    { opis: 'nic', prawdo: 40, rodzaj: [1], sila: 7 },
+    { opis: 'oblesny gnom', prawdo: 10, rodzaj: [1], sila: 2 },
+    { opis: 'wielki smok', prawdo: 5, rodzaj: [1], sila: 7 },
+    { opis: 'straszny troll', prawdo: 10, rodzaj: [1], sila: 3 },
+    { opis: 'zlodzieja', prawdo: 1, rodzaj: [2], sila: 1 },
+    { opis: 'czarodzieja', prawdo: 6, rodzaj: [2], sila: 1 },
+    { opis: 'dobrego duszka', prawdo: 5, rodzaj: [2], sila: 1 },
+    { opis: 'zlego ducha', prawdo: 5, rodzaj: [2], sila: 1 },
+    { opis: 'miecz', prawdo: 2, rodzaj: [3], sila: 1 },
+    { opis: 'zbroje', prawdo: 2, rodzaj: [3], sila: 1 },
+    { opis: 'magiczny eliksir', prawdo: 4, rodzaj: [3], sila: 1 },
+    { opis: 'sztuke miesa', prawdo: 4, rodzaj: [3], sila: 1 },
+    { opis: 'zloto', prawdo: 5, rodzaj: [3], sila: 1 },
+    { opis: 'trucizne', prawdo: 3, rodzaj: [3], sila: 1 }
+];
+var Bohater = (function () {
+    function Bohater(element, wytrzymalosc, sila, bagaz, zloto) {
+        this.element = element;
+        this.wytrzymalosc = wytrzymalosc;
+        this.sila = sila;
+        this.bagaz = bagaz;
+        this.zloto = zloto;
+    }
+    Bohater.prototype.opiszBohatera = function () {
+        // description
+        this.element.innerHTML += "Bohater: wytrzymalosc:" + this.wytrzymalosc + " , bagaz: " + this.bagaz + " , zloto: " + this.zloto;
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+    };
+    return Bohater;
+}());
+;
+var Zdarzenie = (function () {
+    function Zdarzenie(element) {
+        this.element = element;
+        //this.element.innerHTML += "Zdarzenie: ";
+        //this.span = document.createElement('span');
+        //this.element.appendChild(this.span);
+    }
+    Zdarzenie.prototype.spotkajWroga = function (kogo) {
+        // description
+        this.element.innerHTML += "Spotkales: " + swiat[kogo].opis + " , rodzaj: " + swiat[kogo].rodzaj + " , sila: " + swiat[kogo].sila;
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+    };
+    Zdarzenie.prototype.spotkajNieznajomego = function (kogo) {
+        // description
+    };
+    Zdarzenie.prototype.znajdzPrzedmiot = function (jaki) {
+        // description
+    };
+    return Zdarzenie;
+}());
+;
+function fight() {
+    alert('fight');
+}
+function flee() {
+    alert('flee');
+}
+function talk() {
+    alert('talk');
+}
 window.onload = function () {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
     greeter.start();
     var tbl = document.getElementById('tablica');
     var hafn = new Hafn(tbl);
+    var ekran = document.getElementById('ekran');
+    var statystyki = document.getElementById('stat');
+    var bohater = new Bohater(statystyki, 1, 1, [1], 1);
+    bohater.opiszBohatera();
+    var zdarzenie = new Zdarzenie(ekran);
+    var losowanie = Math.floor(Math.random() * 14);
+    zdarzenie.spotkajWroga(losowanie);
 };
 //# sourceMappingURL=app.js.map
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -45,6 +45,104 @@ class Hafn {
     }
 }
 
+let r_zdarzenia: string[] = ['wrog', 'nieznajomy', 'przedmiot', 'nic'];
+let r_nieznajomego: string[] = ['zlodziej', 'dobry_d', 'zly_d', 'czarodziej'];
+let r_przedmiotu: string[] = ['miecz', 'zbroja', 'eliksir', 'mieso', 'zloto', 'trucizna'];
+
+class OpisSwiata {
+    opis: string;
+    prawdo: number;
+    rodzaj: number[];
+    sila: number;
+}
+
+let swiat: Array<OpisSwiata> = [
+    { opis: 'nic', prawdo: 40, rodzaj: [1], sila: 7 },
+    { opis: 'oblesny gnom', prawdo: 10, rodzaj: [1], sila: 2 },
+    { opis: 'wielki smok', prawdo: 5, rodzaj: [1], sila: 7 },
+    { opis: 'straszny troll', prawdo: 10, rodzaj: [1], sila: 3 },
+    { opis: 'zlodzieja', prawdo: 1, rodzaj: [2], sila: 1 },
+    { opis: 'czarodzieja', prawdo: 6, rodzaj: [2], sila: 1 },
+    { opis: 'dobrego duszka', prawdo: 5, rodzaj: [2], sila: 1 },
+    { opis: 'zlego ducha', prawdo: 5, rodzaj: [2], sila: 1 },
+    { opis: 'miecz', prawdo: 2, rodzaj: [3], sila: 1 },
+    { opis: 'zbroje', prawdo: 2, rodzaj: [3], sila: 1 },
+    { opis: 'magiczny eliksir', prawdo: 4, rodzaj: [3], sila: 1 },
+    { opis: 'sztuke miesa', prawdo: 4, rodzaj: [3], sila: 1 },
+    { opis: 'zloto', prawdo: 5, rodzaj: [3], sila: 1 },
+    { opis: 'trucizne', prawdo: 3, rodzaj: [3], sila: 1 }
+    ];
+
+class Bohater {
+    wytrzymalosc: number;
+    sila: number;
+    bagaz: Array<number>; // bagaz: number[];
+    zloto: number;
+    // potrzebne do edycji strony
+    element: HTMLElement;
+    span: HTMLElement;
+
+    constructor(element: HTMLElement, wytrzymalosc: number, sila: number, bagaz: number[], zloto: number) {
+        this.element = element;
+        this.wytrzymalosc = wytrzymalosc;
+        this.sila = sila;
+        this.bagaz = bagaz;
+        this.zloto = zloto;
+    }
+
+    opiszBohatera() {
+        // description
+        this.element.innerHTML += "Bohater: wytrzymalosc:" + this.wytrzymalosc + " , bagaz: " + this.bagaz + " , zloto: " + this.zloto;
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+    }
+
+};
+
+class Zdarzenie {
+    opis: string;
+    prawdo: number;
+    rodzaj: Array<number>; // rodzaj: number[];
+    // potrzebne do edycji strony
+    element: HTMLElement;
+    span: HTMLElement;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        //this.element.innerHTML += "Zdarzenie: ";
+        //this.span = document.createElement('span');
+        //this.element.appendChild(this.span);
+    }
+
+    spotkajWroga(kogo: number) {
+        // description
+        this.element.innerHTML += "Spotkales: " + swiat[kogo].opis + " , rodzaj: " + swiat[kogo].rodzaj + " , sila: " + swiat[kogo].sila;
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+    }
+
+    spotkajNieznajomego(kogo: number) {
+        // description
+    }
+
+    znajdzPrzedmiot(jaki: number) {
+        // description
+    }
+
+};
+
+function fight(): void {
+    alert('fight');
+}
+
+function flee(): void {
+    alert('flee');
+}
+
+function talk(): void {
+    alert('talk');
+}
+
 window.onload = () => {
     var el = document.getElementById('content');
     var greeter = new Greeter(el);
@@ -52,4 +150,13 @@ window.onload = () => {
 
     var tbl = document.getElementById('tablica');
     var hafn = new Hafn(tbl);
+
+    var ekran = document.getElementById('ekran');
+    var statystyki = document.getElementById('stat');
+    var bohater = new Bohater(statystyki,1,1,[1],1);
+    bohater.opiszBohatera();
+    var zdarzenie = new Zdarzenie(ekran);
+    var losowanie = Math.floor(Math.random() * 14);
+    zdarzenie.spotkajWroga(losowanie);
+
 };
