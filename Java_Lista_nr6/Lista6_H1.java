dc5afc7d48fdf7eac2f2f6934f370d3af6a55036
msawicki <msawicki@it-dev.pl>
2018-01-12 19:21:17 +0100

    Poczatek listy 6.

---
+++ Java_Lista_nr6/Lista6_H1.java
@@ -0,0 +1,10 @@
+public class Main {
+	public class Pracownik {
+		String nazwisko;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java_Lista_nr6/Lista6_H2/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+Faktura fakt = new Faktura();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
