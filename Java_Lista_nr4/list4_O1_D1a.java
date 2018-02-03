fa38ec06b30e595a6ee83df01953fd992f47ac60
msawicki <msawicki@it-dev.pl>
2018-02-03 09:56:15 +0100

    Zadanie O1 D1a

--- Java_Lista_nr4/list4_O1_D1a.java
+++ Java_Lista_nr4/list4_O1_D1a.java
@@ -5,67 +5,32 @@
  * @author: Marcel Sawicki
  * @version 1.00 2014/1/9
  */
-class Punkt{
-	double punktX, punktY;
-	double x,y;
-	
-	//setter - metody ustawiajace pola obiektu
-	public void ustawX(double x){
-		this.x=x;
-	}
-	
-	public void ustawY(double y){
-		this.y=y;
-	}
-	//getter - metody odczytujace stan obiektu
-	public double pokazX(){
-		return x;
-	}
-	
-	public double pokazY(){
-		return y;
-	}
-	
-	//metody klasy
-	public double odl(double xa, double xb, double ya, double yb){
-	double dx= xa-xb;
-	double dy= ya-yb;
-	return Math.sqrt(dx*dx+dy*dy);
-    }//odl
-    
-    //metoda sprawdzajaca przynaleznosc punktu do Obszaru nr 1.
-    boolean NalezyDoObszaru1(double x0, double y0){
-    	
-		if(x0>0 && y0>0)
-		{
-			System.out.println("Obszar nr 1.");
-			return true;
-			//double r0=odl(1,x0,1,y0);
-			 //System.out.println("Utworzony promien od srodka okregu (1,1) do P(x,y)="+r0);
-			 //if(r0*r0<=1*1){System.out.println("Punkt nalezy do okrêgu");};
-			 //if(r0*r0>1*1 && x0<=2 && y0<=2)
-			 //	{System.out.println("Punkt nie nalezy do okrêgu, ale nalezy do kwadratu.");}
-			//	else{System.out.println("Punkt nie nalezy do okrêgu oraz nie nalezy do kwadratu.");};
-		}
-		else
-		{
-			return false;  
-		}
-    }
-}
 
-public class List4O1D1a {
+import java.util.*;
+
+public class List4_O1_D1a {
 	public static void main(String[] args){
 		//tworze obiekt klasy Punkt	
 		Punkt punktMoj=new Punkt();
+		System.out.println("Prosze podac wspolrzedne punktu A: ");
 		//pobieram wspolrzedne od uzytkownika
-		punktMoj.ustawX(Czytanie.czytajDouble("x="));
-		punktMoj.ustawY(Czytanie.czytajDouble("y="));
-		punktMoj.odl(punktMoj.pokazX(),2,punktMoj.pokazY(),4);
-		System.out.println(punktMoj.odl(punktMoj.pokazX(),2,punktMoj.pokazY(),4));
-		punktMoj.NalezyDoObszaru1(punktMoj.pokazX(),punktMoj.pokazY());
-	}
+		Scanner kx = new Scanner(System.in);
+		System.out.print("x= ");
+		double zmiennaX = kx.nextDouble();
 	
-    
-    
+		
+		Scanner ky = new Scanner(System.in);
+		System.out.print("y= ");
+		double zmiennaY = ky.nextDouble();
+	
+		punktMoj.ustawX(zmiennaX);
+		punktMoj.ustawY(zmiennaY);
+		System.out.println("Obliczam odleglosc punktu A od poczatku ukladu wspolrzednych.");
+		double odlegloscX0Y = punktMoj.odl(punktMoj.pokazX(),0,punktMoj.pokazY(),0);
+		System.out.println(odlegloscX0Y);
+		System.out.println("Sprawdzam czy punktu A nalezy do obszaru nr 1.");
+		punktMoj.NalezyDoObszaru1(punktMoj.pokazX(),punktMoj.pokazY());
+		kx.close();
+		ky.close();
+	}
 }
