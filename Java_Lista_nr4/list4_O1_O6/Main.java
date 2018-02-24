cb6c6d1cb660049a4a04a6f859cac058222c076f
msawicki <msawicki@it-dev.pl>
2018-02-24 19:20:45 +0100

    Collection.

--- Java_Lista_nr4/list4_O1_O6/Main.java
+++ Java_Lista_nr4/list4_O1_O6/Main.java
@@ -18,12 +18,33 @@
  *  - zwolnij wszystkie pokoje wynajmowane przez osobê o podanym nazwisku. 
  *  Zaproponuj ewentualne inne metody klasy Hotel
  */
+import java.util.LinkedList;
+import java.util.List;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Hotel grand = new Hotel(2,3); // liczba pieter, liczba pokoi
 		Osoba osoba1 = new Osoba("Jan", "Abcd"); // imie, nazwisko
 		Osoba osoba2 = new Osoba("Mariusz", "Abcd"); // imie, nazwisko
+		List<Osoba> kolekcjaOsob = new LinkedList<>();
+		kolekcjaOsob.add(osoba1);
+		kolekcjaOsob.add(osoba2);
+		
+		// iterowanie po kolekcji
+		
+		System.out.println("Iterowanie po liscie (foreach)");
+		for(Osoba item : kolekcjaOsob) {
+		    System.out.println(item.imie);
+		    System.out.println(item.nazwisko);
+		}
+		 
+		System.out.println("Iterowanie po liscie (for)");
+		for(int index = 0; index < kolekcjaOsob.size(); index++) {
+		    System.out.println(kolekcjaOsob.get(index).imie);
+		    System.out.println(kolekcjaOsob.get(index).nazwisko);
+		}
+		
+		
 		grand.wynajmijPokoj(1, osoba1);
 		
 		grand.informacjePokoj(1);
