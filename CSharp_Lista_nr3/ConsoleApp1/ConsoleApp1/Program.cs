85348f02de1c9a66ac695acfddcf6ef1c70aaa9d
msawicki <msawicki@it-dev.pl>
2018-04-06 21:19:41 +0200

    CSharp List 3

---
+++ CSharp_Lista_nr3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Towar towar = new Towar();
+
+            Towar towar1 = new Towar("Woda") { nazwa = "Woda", ilosc = 2, cenaNetto = 2, typ = "Napoj" };
+            Towar towar2 = new Towar("Sok") { nazwa = "Sok", ilosc = 3, cenaNetto = 22, typ = "Napoj" };
+            System.Console.WriteLine(towar1);
+            System.Console.WriteLine(towar2);
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp_Lista_nr3/ConsoleApp1/ConsoleApp1/Towar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Towar
+    {
+
+        // nazwa, cena netto, typ, ilość w magazynie
+        public string nazwa;
+        public float cenaNetto;
+        public string typ;
+        public int ilosc;
+
+        public Towar() { }
+        public Towar(string nazwa)
+        {
+            this.nazwa = nazwa;
+        }
+        public Towar(string nazwa, string typ)
+        {
+            this.nazwa = nazwa;
+            this.typ = typ;
+        }
+        public Towar(string nazwa, string typ, string cenaNetto, string ilosc) 
+        {
+            this.nazwa = nazwa;
+            this.typ = typ;
+
+        }
+        public override string ToString()
+        {
+            return nazwa + "\t\t|" + ilosc + "\t|" + cenaNetto + "\t\t|" + typ;
+        }
+    }
+}
