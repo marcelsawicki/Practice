3300be3f11d47fddfd8bd8c896d8cddd33b2bca3
msawicki <msawicki@it-dev.pl>
2018-04-18 10:15:14 +0200

    async

---
+++ CSharp_async/ConsoleApp6LockDemonstration/ConsoleApp6LockDemonstration/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6LockDemonstration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Task> list = new List<Task>();
+            ReadClass.HowMuchWillBeDone = 6;
+            list.Add(new Task(ReadClass.SaveLockMessage));
+            list.Add(new Task(ReadClass.SaveLockMessage));
+            list.Add(new Task(ReadClass.SaveLockMessage));
+            list.Add(new Task(ReadClass.SaveLockMessage));
+            list.Add(new Task(ReadClass.SaveLockMessage));
+            list.Add(new Task(ReadClass.SaveLockMessage));
+
+            foreach (var task in list)
+            {
+                task.Start();
+            }
+
+            while (!ReadClass.CheckIsDone())
+            { }
+
+            Console.WriteLine("\n Skończone...");
+
+            Console.ReadKey();
+        }
+    }
+
+    class ReadClass
+    {
+        private static readonly object SyncObject = new object();
+        private static TextWriter W;
+
+        public static void SaveLockMessage()
+        {
+            Console.WriteLine("Przed Lockiem");
+
+            lock (SyncObject)
+            {
+                i++;
+                W = new StreamWriter("log.txt", true);
+                Console.WriteLine("\t Start zapisu do pliku: " + i);
+                W.WriteLine("Otrzymana: {0} {1}",
+                    DateTime.Now.ToLongTimeString(),
+                    DateTime.Now.ToLongDateString());
+                Thread.Sleep(1000);
+
+                Console.WriteLine("\t Skończyłem: " + i);
+                W.Close();
+            }
+        }
+
+        private static int i = 0;
+
+        public static int HowMuchWillBeDone = 6;
+
+        public static bool CheckIsDone()
+        {
+            return i >= HowMuchWillBeDone;
+        }
+    }
+
+}
---
+++ CSharp_async/ConsoleApp6MyFirstAsync/ConsoleApp6MyFirstAsync/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleApp6MyFirstAsync
+{
+    class Program
+    {
+                static void Main(string[] args)
+                {
+                    MyThreadClass oMyThreadClass = new MyThreadClass();
+                    Thread oThread = new Thread(new ThreadStart(oMyThreadClass.Run));
+                    oThread.Start();
+                    Console.WriteLine("Stan dodanego wątku:" + oThread.ThreadState);
+                    Console.WriteLine("Priorytet dodanego wątku:" + oThread.Priority);
+                    Console.WriteLine("Oczekiwanie na zakończenie wątku...");
+                    oThread.Join();
+                    Console.WriteLine("Stan głównego wątku:"+Thread.CurrentThread.ThreadState);
+                    Console.WriteLine("Priorytet głównego wątku:" + Thread.CurrentThread.Priority);
+                    Console.WriteLine("Stan dodanego wątku:" + oThread.ThreadState);
+                    Console.ReadKey();
+                }
+            }
+
+            class MyThreadClass
+            {
+            public MyThreadClass()
+            {
+             }
+
+        public void Run()
+        {
+            Console.WriteLine("Rozpoczynam pracę...");
+            // Jakieś długie operacje
+            Console.WriteLine("Test uśpienia wątku...");
+            Thread.Sleep(500);
+            Console.WriteLine("Kończę pracę...");
+        }
+    }
+}
