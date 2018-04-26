dea419ea73d2ac907f751153bd2f2594da8a1e19
annamdr <annamadrecka@gmail.com>
2018-04-26 08:02:00 +0200

    Lista 3 Poprawki

---
+++ CSharp_Lista_nr3/ConsoleApp1/ConsoleApp1/Swap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Swap
+	{
+		public void swap(ref int liczbaA, ref int liczbaB)
+		{
+			System.Console.WriteLine("Metoda swap - zamiana liczb.");
+
+			int zmiennaPomocnicza = liczbaB;
+			liczbaB = liczbaA;
+			liczbaA = zmiennaPomocnicza;
+		}
+	}
+}
