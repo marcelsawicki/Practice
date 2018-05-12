aa120840a155330850b1706bab091f1c6431466f
msawicki <msawicki@it-dev.pl>
2018-05-12 13:55:08 +0200

    Zadanie 5

---
+++ CSharp_Lista_Nr5/WebApplication2/WebApplication2/Models/Gracz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Gracz
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ CSharp_Lista_Nr5/WebApplication2/WebApplication2/Models/GraczContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class GraczContext:DbContext
+    {
+        public GraczContext():base("GraczConnection")
+        {
+        }
+        public DbSet<Gracz> Graczs { get; set; }
+
+    }
+}
