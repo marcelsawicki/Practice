9e3a67ffd425e73a2b1490e2596e8c3db28d6fe9
Marcel Sawicki <marcel.sawicki@gmail.com>
2018-08-21 18:38:16 +0200

    petla for

--- Arduino/leds.c
+++ Arduino/leds.c
@@ -1,36 +1,28 @@
+int d = 100;
 void setup()
 {
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(2, HIGH);
-  delay(500);
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
-  delay(500);
-  digitalWrite(3, LOW);
-  digitalWrite(4, HIGH);
-  delay(500);
-  digitalWrite(4, LOW);
-  digitalWrite(5, HIGH);
-  delay(500);
-  digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);
-  delay(500);
-  digitalWrite(6, LOW);
-  digitalWrite(5, HIGH);
-  delay(500);
-  digitalWrite(5, LOW);
-  digitalWrite(4, HIGH);
-  delay(500);
-  digitalWrite(4, LOW);
-  digitalWrite(3, HIGH);
-  delay(500);
-  digitalWrite(3, LOW);
+for(int a = 2; a < 7; a++)
+  {
+    digitalWrite(a, HIGH);
+    delay(d);
+    digitalWrite(a, LOW);
+    delay(d);
+  }
+
+  for(int b = 7; b > 1; b--)
+  {
+    digitalWrite(b, HIGH);
+    delay(d);
+    digitalWrite(b, LOW);
+    delay(d);
+  }
 }
