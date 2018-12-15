d29202adf5c6fc34d82de68ca2b1d4925920edcf
Marcel Sawicki <marcel.sawicki@gmail.com>
2018-12-15 08:11:58 +0100

    Regex adres e-mail

---
+++ CSharp_Programy/Sandbox/Regex-adres-email/ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regEmail;
+            regEmail = new Regex(@"^[a-z][a-z0-9_]*@[a-z0-9]*\.[a-z]{2,3}$");
+            Console.WriteLine("Prosze podac adres email: ");
+
+            string napis = Console.ReadLine();
+
+            // Podany przez uzytkownika
+            Console.WriteLine(
+                String.Format("Podany adres: {0} to {1}poprawny adres e-mail.", napis, regEmail.IsMatch(napis)?"":"nie")
+                );
+            Console.ReadKey();
+        }
+    }
+}
