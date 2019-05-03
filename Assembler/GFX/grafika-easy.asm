5fdbf7cd4c2e0ff084b7166aab22c6bbbe97b69b
Marcel Sawicki <marcel.sawicki@gmail.com>
2019-05-03 19:48:40 +0200

    wyswietlenie pixel'a

---
+++ Assembler/GFX/grafika-easy.asm
@@ -0,0 +1,32 @@
+.model small
+.data
+x dw 154
+y dw 100
+kolor db 7
+.code
+start:
+.486
+	mov ax,seg x
+	mov ds,ax
+	mov ax,13h
+	int 10h
+	mov ax,0A000h
+	mov es,ax
+	mov ax,y
+	mov di,320
+	mul di
+	add ax,x
+	add di,ax
+	mov al,kolor
+	mov byte ptr es:[di],al
+
+petla:
+	in al,60h
+	cmp al, 1
+	jne petla
+	mov ax,3
+	int 10h
+	mov ah,4ch
+	int 21h
+end start
+end
