cb387988457141bcada88f3e1cf6117b7605833e
Marcel Sawicki <msawicki@it-dev.pl>
2019-08-07 07:11:52 +0200

    Listing 1. Główny skrypt w C# zawarty w zmiennej PS

---
+++ PowerShell/Gazeta/Listing1-skrypt-C-sharp-zawarty-w-zmiennej-PS.ps1
@@ -0,0 +1,32 @@
+﻿# Główny skrypt w C# zawarty w zmiennej PS
+$script=@"
+using System;
+using System.IO;
+using System.Net;
+
+namespace Example
+{
+    public class WebRequestGet
+    {
+        public static void Main()
+        {
+            WebRequest request = WebRequest.Create("https://programistamag.pl");
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            using(Stream dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+            }
+            response.Close();
+        }
+    }
+}
+"@
+# Dodanie typu dla zmiennej przetrzymującej skrypt w C#
+Add-Type -TypeDefinition $script -Language CSharp
+
+#Wywołanie skryptu C#
+[Example.WebRequestGet]::Main()
