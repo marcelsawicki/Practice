7d4073837fbb976aff6a76cc61decbc23711b331
Marcel Sawicki <msawicki@it-dev.pl>
2020-02-26 09:09:12 +0100

    Init.

---
+++ TypeScript/IOT/Zadanie2/src/app.ts
@@ -0,0 +1,18 @@
+// app.ts
+
+export class Hero {
+    id: number | undefined;
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    myName()
+    {
+        return this.name;
+    }
+  }
+
+  let hero = new Hero('krunal');
+  console.log(hero.myName());
