d814b9ee9a6d57c24956af68ca17c8466e235b0a
Marcel Sawicki <msawicki@it-dev.pl>
2020-05-25 17:38:32 +0200

    callback

---
+++ callback.ts
@@ -0,0 +1,24 @@
+function delayedResponseWithCallback(callback: Function)
+{
+    function delayedAfterTimeout()
+    {
+        console.log('delayedAfterTimeout');
+        callback();
+    }
+
+    setTimeout(delayedAfterTimeout, 1000);
+}
+
+function callDelayedAndWait()
+{
+    function afterWait()
+    {
+        console.log('afterWait');
+    }
+
+    console.log(`przed wywołaniem funkcji delayedResponseWithCallback`);
+    delayedResponseWithCallback(afterWait);
+    console.log(`po wywołaniu funkcji calling delayedResponseWithCallback`);
+}
+
+callDelayedAndWait();
