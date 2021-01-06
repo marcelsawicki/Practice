573842da4ef52567a7f97eb5c184525826f041b5
Marcel Sawicki <msawicki@it-dev.pl>
2021-01-06 16:56:56 +0100

    Async

---
+++ Concurrency/AsyncApp1/AsyncApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AsyncApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = Service.Process();
+            task.Wait();
+        }
+    }
+}
---
+++ Concurrency/AsyncApp1/AsyncApp1/Service.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncApp1
+{
+    public static class Service
+    {
+        public static async Task Process()
+        {
+            int val = 13;
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Console.WriteLine(val);
+
+            val += 2;
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+
+            Console.WriteLine(val);
+        }
+    }
+}
