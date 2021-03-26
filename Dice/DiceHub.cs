45c383d8185e1699e242c80990e0e207c2588f26
Marcel Sawicki <msawicki@it-dev.pl>
2021-03-26 10:57:05 +0100

    Dice

---
+++ Dice/DiceHub.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dice
+{
+    public class Roll
+    {
+        static Random Random = new Random();
+
+        public string Username { get; set; }
+        public DateTime Time { get; set; }
+        public int DiceSize { get; set; }
+        public int Value { get; set; }
+
+        public Roll(string username, int diceSize)
+        {
+            Username = username;
+            Time = DateTime.Now;
+            DiceSize = diceSize;
+            Value = Random.Next(DiceSize) + 1;
+        }
+    }
+
+    public class DiceHub : Hub
+    {
+        static Dictionary<string, string> UsernameByClientId = new Dictionary<string, string>();
+        static List<Roll> Rolls = new List<Roll>();
+
+        bool HasUsername(out string username)
+        {
+            username = Context.GetHttpContext().Request.Cookies["dice_username"];
+            return string.IsNullOrWhiteSpace(username) == false;
+        }
+
+        public async Task Roll(int diceSize)
+        {
+            if (HasUsername(out string username))
+            {
+                Roll roll = new Roll(username, diceSize);
+                Rolls.Add(roll);
+                await Clients.All.SendAsync("onRolled", roll);
+            }
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            if (HasUsername(out string username)) 
+            {
+                lock (UsernameByClientId)
+                {
+                    if (UsernameByClientId.ContainsValue(username))
+                    {
+                        Clients.Caller.SendAsync("onAlert", $"Username '{username}' is already used.");
+                        return;
+                    }
+                    else
+                    {
+                        UsernameByClientId[Context.ConnectionId] = username;
+                    }
+                }
+
+                await Clients.Caller.SendAsync("onWelcome", UsernameByClientId.Values, Rolls);
+                await Clients.All.SendAsync("onJoined", username);
+            }
+
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (UsernameByClientId.TryGetValue(Context.ConnectionId, out string username))
+            {
+                lock (UsernameByClientId)
+                {
+                    UsernameByClientId.Remove(Context.ConnectionId);
+                }
+
+                await Clients.All.SendAsync("onLeft", username);
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ Dice/wwwroot/main.js
@@ -0,0 +1,92 @@
+﻿var connection = new signalR.HubConnectionBuilder().configureLogging(signalR.LogLevel.Debug).withUrl("/dice").build();
+connection.on("onWelcome", onWelcome);
+connection.on("onRolled", onRolled);
+connection.on("onJoined", onJoined);
+connection.on("onLeft", onLeft);
+connection.on("onAlert", onAlert);
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+async function onLoad() {
+    view_login.classList.remove('active');
+    view_room.classList.remove('active');
+    let username = getCookie('dice_username');
+    if (username == '') {
+        view_login.classList.add('active');
+        return;
+    }
+    view_room.classList.add('active');
+    onJoined(username, true);
+    connection.start();
+}
+
+function onWelcome(users, rolls) {
+    users.forEach(onJoined);
+    rolls.forEach(onRolled);
+}
+
+function onJoined(user, me = false) {
+    let userElement = document.getElementById(`user_${user}`)
+    if (!userElement) {
+        userElement = document.createElement('div');
+        userElement.id = `user_${user}`;
+        userElement.innerText = user;
+        if (me) {
+            userElement.classList.add('me');
+        }
+        users.appendChild(userElement);
+    }
+}
+
+function onLeft(user) {
+    document.getElementById(`user_${user}`)?.remove();
+}
+
+function onRolled({ username, time, diceSize, value }) {
+    let messageElement = document.createElement('div');
+    messageElement.innerHTML = `<span class="date">${new Date(time).toLocaleTimeString()}</span> <b></b> rolled d${diceSize} <b class="value">${value}</b>`;
+    messageElement.querySelector('b').innerText = username;
+    content.appendChild(messageElement);
+    content.scrollTop = content.scrollHeight;
+}
+
+function onAlert(message) {
+    alert(message);
+}
+
+function login() {
+    if (username.value != '') {
+        setCookie('dice_username', username.value, 1);
+        onLoad();
+    }
+}
+
+function logout() {
+    setCookie('dice_username', '', 1);
+    window.location.reload();
+}
+
+function roll() {
+    connection.invoke("Roll", parseInt(dice_size.value));
+}
