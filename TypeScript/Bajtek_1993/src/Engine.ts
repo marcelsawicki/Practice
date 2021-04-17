603b0d387916d3a03da1c18475a09d0fe4cfaa52
Marcel Sawicki <msawicki@it-dev.pl>
2021-04-17 09:31:49 +0200

    Update

--- TypeScript/Bajtek_1993/src/Engine.ts
+++ TypeScript/Bajtek_1993/src/Engine.ts
@@ -12,7 +12,7 @@ export class Engine {
     
     describeHero(hero: Hero){
         // describe Hero - toughness, strength, luggage, gold
-        let container = document.getElementById('main');
+        let container = document.getElementById('content');
         let childContainer = document.createElement('div');
         childContainer.innerText = 'Wytrzymalosc: ' + hero.toughness;
         container?.appendChild(childContainer);
@@ -33,27 +33,66 @@ export class Engine {
 
         container?.appendChild(childContainer4);
     }
+    
+    dispatchEvent(){
+        let randomNumber = Math.floor(Math.random()*this.storyEventData.length);
+        let randomEvent = this.storyEventData[randomNumber];
 
-    meetEnemy(){
+        switch(randomEvent.typeOfEvent) { 
+            case "wrog": { 
+               //statements; 
+               this.meetEnemy(randomNumber)
+               break; 
+            } 
+            case "nieznajomy": { 
+               //statements; 
+               this.meetStranger(randomNumber);
+               break; 
+            } 
+            case "przedmiot": { 
+                this.findItem(randomNumber);
+                //statements; 
+                break; 
+             } 
+            default: { 
+               //statements; 
+               break; 
+            } 
+         }
+    }
+
+    meetEnemy(randomNumber: number){
         // meet enemy
-        let message = this.storyEventData[Math.floor(Math.random()*this.storyEventData.length)].description;
-        let container = document.getElementById('main');
+        let enemyDescription = this.storyEventData[randomNumber].description;
+        let enemyStrength = this.storyEventData[randomNumber].strength;
+        let container = document.getElementById('content');
         let childContainer = document.createElement('div');
-        childContainer.innerText = message;
+        childContainer.innerText = 'Zza krzkow wyskoczyl ' + enemyDescription + ' o sile: ' + enemyStrength + '.';
+        container?.appendChild(childContainer);
+        childContainer.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+
+    meetStranger(randomNumber: number){
+        // meet stranger
+        let message = this.storyEventData[randomNumber].description;
+        let container = document.getElementById('content');
+        let childContainer = document.createElement('div');
+        childContainer.innerText = 'Spotkales ' + message + '.';
         container?.appendChild(childContainer);
     }
 
-    meetStranger(){
-        // meet stranger
-    }
-
-    findItem(){
+    findItem(randomNumber: number){
         // find item
+        let message = this.storyEventData[randomNumber].description;
+        let container = document.getElementById('content');
+        let childContainer = document.createElement('div');
+        childContainer.innerText = 'Znalazles ' + message + '.';
+        container?.appendChild(childContainer);
     }
 
     showMessage(message: string)
     {
-        let container = document.getElementById('main');
+        let container = document.getElementById('content');
         let childContainer = document.createElement('div');
         childContainer.innerText = message;
         container?.appendChild(childContainer);
--- TypeScript/Bajtek_1993/src/Main.ts
+++ TypeScript/Bajtek_1993/src/Main.ts
@@ -5,13 +5,12 @@ window.onload = () => {
 	let engine: Engine = new Engine(1);
 	let hero: Hero = new Hero(1,2,3,["miecz","sakiewka"],1);
 
-	let button1 = document.getElementById('button1');
+	let button1 = document.getElementById('hero-describe');
 	button1?.addEventListener("click", function(){ engine.describeHero(hero) }, false);
 
-	let button2 = document.getElementById('button2');
-	button2?.addEventListener("click", function(){ engine.meetEnemy() }, false);
+	let button2 = document.getElementById('go');
+	button2?.addEventListener("click", function(){ engine.dispatchEvent() }, false);
 
 	let end:boolean = false;
 	let ile: number = 0;
-	engine.showMessage('hello world');
 }
