21cf6464f710d79676c27a3f648de0cb0a55181c
Marcel Sawicki <marcel.sawicki@gmail.com>
2021-09-23 16:50:26 +0200

    ServiceBus

---
+++ CSharp/ServiceBus/ServiceBusApp1/ServiceBusApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace ServiceBusApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // using Azure.Messaging.ServiceBus;
+
+            string connectionString = "<connection_string>";
+            string queueName = "<queue_name>";
+
+            // Because ServiceBusClient implements IAsyncDisposable, we'll create it 
+            // with "await using" so that it is automatically disposed for us.
+            await using var client = new ServiceBusClient(connectionString);
+
+            // The sender is responsible for publishing messages to the queue.
+            ServiceBusSender sender = client.CreateSender(queueName);
+            ServiceBusMessage message = new ServiceBusMessage("Hello world!");
+
+            await sender.SendMessageAsync(message);
+
+            // The receiver is responsible for reading messages from the queue.
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+            ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+            string body = receivedMessage.Body.ToString();
+            Console.WriteLine(body);
+        }
+    }
+}
