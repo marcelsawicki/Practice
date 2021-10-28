3872b00b8ef8b38fa810126686029fab8cb1c9b8
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2021-10-28 18:20:53 +0200

    Dodanie Webpack serve oraz index.html

--- TypeScript/SSR/ReactApp1/webpack.config.js
+++ TypeScript/SSR/ReactApp1/webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   devtool: "source-map",
   module: {
@@ -14,5 +17,23 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       }
     ]
-  }
+  },
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+     title: 'Output Management',
+     title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })
+  ],
 };
