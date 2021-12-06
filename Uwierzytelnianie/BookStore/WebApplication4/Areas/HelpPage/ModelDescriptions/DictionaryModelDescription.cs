0f0c21352f46325f721b8f1cea59ab9e5433431d
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2021-12-06 19:55:44 +0100

    BookStore

---
+++ Uwierzytelnianie/BookStore/WebApplication4/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookStoreApp.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Uwierzytelnianie/BookStore/WebApplication4/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BookStoreApp.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
