ccebe55d7bf19bb334fe21a3fe35f48466527eb0
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2021-12-15 19:17:33 +0100

    pl_2

---
+++ C++/pl_2.cpp
@@ -0,0 +1,26 @@
+/* pl_2.cpp Obliczanie wynagrodzenia dla autora za sprzedane ksiazki. */
+#include <conio.h>
+#include <iostream.h>
+#define P 0.15 //definicja stalej rzeczywistej - procent dla autora
+#define F 0.19 //procent podatku
+
+void main(void)
+{
+	int l, c;
+	
+	float wb, wn, pn;
+	
+	cout<<"\n Obliczanie wynagrodzenia dla autora";
+	cout<<"\n Podaj cene ksiazki ";
+	cin>>c;
+	cout<<" Podaj liczbe sprzedanych egzeplarzy ";
+	cin>>l;
+	wb=P*c*l; 	//obliczenie wynagrodzenia brutto
+	pn=wb/2*F; 	//obliczenie podatku
+	wn=wb-pn;	//obliczenie wynagrodzenia netto
+	cout<<"\n Wynagrodzenie brutto dla autora wynosi "<<wb;
+	cout<<"\n Podatek od wynagrodzenia dla autora wynosi"<<pn;
+	cout<<"\n Wynagrodzenie netto dla autora wynosi"<<wn;
+	cout<<"\n Nacisnij dowolny klawisz"<<endl;
+	getch();
+}
