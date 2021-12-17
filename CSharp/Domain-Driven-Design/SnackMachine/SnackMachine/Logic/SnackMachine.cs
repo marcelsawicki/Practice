6f9628fac6cbf39c1713aea59818c5e118b2a4e3
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2021-12-17 20:55:24 +0100

    SnackMachine

--- CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Logic/SnackMachine.cs
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Logic/SnackMachine.cs
@@ -11,14 +11,48 @@ namespace Logic
         public int FiveDollarCount { get; private set; }
         public int TwentyDollarCount { get; private set; }
 
+        public int OneCentCountInTransacton { get; private set; }
+        public int TenCentCountInTransacton { get; private set; }
+        public int QuarterCountInTransacton { get; private set; }
+        public int OneDollarCountInTransacton { get; private set; }
+        public int FiveDollarCountInTransacton { get; private set; }
+        public int TwentyDollarCountInTransacton { get; private set; }
+
         public void InsertMoney(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
         {
-            OneCentCount += oneCentCount;
-            TenCentCount += tenCentCount;
-            QuarterCount += quarterCount;
-            OneDollarCount += oneDollarCount;
-            FiveDollarCount += fiveDollarCount;
-            TwentyDollarCount += twentyDollarCount;
+            OneCentCountInTransacton += oneCentCount;
+            TenCentCountInTransacton += tenCentCount;
+            QuarterCountInTransacton += quarterCount;
+            OneDollarCountInTransacton += oneDollarCount;
+            FiveDollarCountInTransacton += fiveDollarCount;
+            TwentyDollarCountInTransacton += twentyDollarCount;
+        }
+
+        public void ReturnMoney()
+        {
+            OneCentCountInTransacton = 0;
+            TenCentCountInTransacton = 0;
+            QuarterCountInTransacton = 0;
+            OneDollarCountInTransacton = 0;
+            FiveDollarCountInTransacton = 0;
+            TwentyDollarCountInTransacton = 0;
+        }
+
+        public void BuySnack() 
+        {
+            OneCentCount += OneCentCountInTransacton;
+            TenCentCount += TenCentCountInTransacton;
+            QuarterCount += QuarterCountInTransacton;
+            OneDollarCount += OneDollarCountInTransacton;
+            FiveDollarCount += FiveDollarCountInTransacton;
+            TwentyDollarCount += TwentyDollarCountInTransacton;
+
+            OneCentCountInTransacton = 0;
+            TenCentCountInTransacton = 0;
+            QuarterCountInTransacton = 0;
+            OneDollarCountInTransacton = 0;
+            FiveDollarCountInTransacton = 0;
+            TwentyDollarCountInTransacton = 0;
         }
     }
 
