d4e3e5197573712486e5d170593f1385f93846a3
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2021-12-17 19:54:58 +0100

    SnackMachine

---
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Logic/SnackMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Logic
+{
+    public sealed class SnackMachine
+    {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int OneDollarCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public void InsertMoney(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            OneCentCount += oneCentCount;
+            TenCentCount += tenCentCount;
+            QuarterCount += quarterCount;
+            OneDollarCount += oneDollarCount;
+            FiveDollarCount += fiveDollarCount;
+            TwentyDollarCount += twentyDollarCount;
+        }
+    }
+
+}
