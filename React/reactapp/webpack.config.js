990cb0ff2452582f6cabab5b461b85a24aef53f3
Marcel Sawicki <marcel.sawicki@gmail.com>
2021-12-21 14:12:37 +0100

    Dodanie React oraz Babel

--- React/reactapp/webpack.config.js
+++ React/reactapp/webpack.config.js
@@ -1,12 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const APP_DIR = path.resolve(__dirname, '');
+const APP_DIR = path.resolve(__dirname, 'ClientApp');
 const BUILD_DIR = path.resolve(__dirname, 'public');
-const BUILD_DIR_BUILD = path.resolve(__dirname, 'public');
 
 const config = {
-    entry: APP_DIR + '/HelloWorld.js',
+    mode: 'development',
+    entry: APP_DIR + '/Client.js',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
@@ -16,7 +16,26 @@ const config = {
            directory: path.join(__dirname, 'public'),
        },
        port: 9000 
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react', 
+                        'stage-2',
+                        ['env', { targets: { browsers: ['last 2 versions'] }}]
+                    ]
+                }
+            }
+        ]
+
     }
+
 };
 
 module.exports = config;
