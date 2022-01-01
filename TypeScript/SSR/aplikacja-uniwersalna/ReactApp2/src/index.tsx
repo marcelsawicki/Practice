48982e2e9403b0f1fbd75a93c244eb9a02b9e59f
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-01-01 12:43:38 +0100

    Props

--- TypeScript/SSR/aplikacja-uniwersalna/ReactApp2/src/index.tsx
+++ TypeScript/SSR/aplikacja-uniwersalna/ReactApp2/src/index.tsx
@@ -2,4 +2,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {App} from "./App";
 
-ReactDOM.render(<App num={1337} />, document.getElementById('root'));
+//ReactDOM.render(<App num={1337} />, document.getElementById('root'));
+
+interface MessageProps {
+    text: string;
+    important: boolean;
+  }
+   
+  function Message({ text, important }: MessageProps) {
+    return (
+      <div>
+        {important ? 'Important message: ' : 'Regular message: '}
+        {text}
+      </div>
+    );
+  }
+ReactDOM.render(<Message
+    text="The form has been submitted!"
+    important={false}
+  />, document.getElementById('root'));
