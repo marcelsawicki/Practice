be0f6797f3277e352172ffca5e24e9c71a92e35b
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-01-05 15:32:12 +0100

    Test

---
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Tests/MoneySpecs.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using Logic;
+using FluentAssertions;
+
+namespace Tests
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_money_produces_correct_result()
+        {
+            //Arrange
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            //Act
+            Money sum = money1 + money2;
+
+            //Assert
+            sum.OneCentCount.Should().Be(2); 
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+        }
+
+        [Fact]
+        public void Two_money_instances_equal_if_contain_the_same_money_amount()
+        {
+            Money money1 = new Money(1, 2, 3, 4, 5, 6);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+            money1.GetHashCode().Should().Be(money2.GetHashCode());
+        }
+    }
+}
