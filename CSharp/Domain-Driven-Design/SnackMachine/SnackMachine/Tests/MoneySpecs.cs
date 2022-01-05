aa91773cb176d1552bfdeb4fec98cee14cd397c4
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-01-05 16:38:44 +0100

    Tests

--- CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Tests/MoneySpecs.cs
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Tests/MoneySpecs.cs
@@ -18,7 +18,7 @@ namespace Tests
             Money sum = money1 + money2;
 
             //Assert
-            sum.OneCentCount.Should().Be(2); 
+            sum.OneCentCount.Should().Be(2);
             sum.TenCentCount.Should().Be(4);
             sum.QuarterCount.Should().Be(6);
             sum.OneDollarCount.Should().Be(8);
@@ -34,6 +34,30 @@ namespace Tests
 
             money1.Should().Be(money2);
             money1.GetHashCode().Should().Be(money2.GetHashCode());
+        }
+
+        [Fact]
+        public void Two_money_instances_do_not_equal_if_contain_different_money_amount()
+        {
+            Money dollar = new Money(0, 0, 0, 1, 0, 0);
+            Money hundredCents = new Money(100, 0, 0, 0, 0, 0);
+
+            dollar.Should().NotBe(hundredCents);
+            dollar.GetHashCode().Should().NotBe(hundredCents.GetHashCode());
+        }
+
+        [Theory]
+        [InlineData(-1, 0, 0, 0, 0, 0)]
+        [InlineData(0, -2, 0, 0, 0, 0)]
+        [InlineData(0, 0, -3, 0, 0, 0)]
+        [InlineData(0, 0, 0, -4, 0, 0)]
+        [InlineData(0, 0, 0, 0, -5, 0)]
+        [InlineData(0, 0, 0, 0, 0, -6)]
+        public void Cannot_create_money_with_negative_value(int oneCentCount, int tenCentCount, int quarterCount, int oneDollarCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            Action action = () => new Money(oneCentCount, tenCentCount, quarterCount, oneDollarCount, fiveDollarCount, twentyDollarCount);
+
+            action.Should().Throw<InvalidOperationException>();
         }
     }
 }
