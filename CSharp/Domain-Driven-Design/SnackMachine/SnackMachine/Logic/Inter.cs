59ca013ffd6b3fbbb2317a7bbc10be68ab6bc4a4
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-01-10 13:34:13 +0100

    Persistence layer

---
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/Logic/Inter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public static class Inter
+    {
+        public static void Init(string connectionString)
+        {
+            
+        }
+    }
+}
