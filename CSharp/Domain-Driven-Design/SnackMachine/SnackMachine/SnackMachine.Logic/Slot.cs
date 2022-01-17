3f49d38431470935790ef4809cbdb4245733efdc
Marcel Sawicki <marcel.sawicki@gmail.com>
2022-01-17 16:25:22 +0100

    Slot and Snack

---
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/SnackMachine.Logic/Slot.cs
@@ -0,0 +1,32 @@
+﻿using SnackMachine.Logic;
+using SnackMachineApp.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackMachineApp.Logic
+{
+    public class Slot : Entity
+    {
+        public virtual Snack Snack { get; protected set; }
+        public virtual int Quantity { get; protected set; }
+        public virtual decimal Price { get; protected set; }
+        public virtual SnackMachine SnackMachine { get; protected set; }
+        public virtual int Position { get; protected set; }
+
+        public Slot()
+        {
+        }
+
+        public Slot(SnackMachine snackMachine, int position, Snack snack, int quantity, decimal price) : this()
+        {
+            SnackMachine = snackMachine;
+            Position = position;
+            Snack = snack;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
---
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/SnackMachine.Logic/Snack.cs
@@ -0,0 +1,23 @@
+﻿using SnackMachineApp.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackMachine.Logic
+{
+    public class Snack : Entity
+    {
+        public virtual string Name { get; protected set; }
+
+        protected Snack()
+        { 
+        }
+
+        public Snack(string name) : this()
+        {
+            Name = name;
+        }
+    }
+}
