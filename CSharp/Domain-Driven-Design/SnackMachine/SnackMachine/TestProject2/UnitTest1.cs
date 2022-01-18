b11376aeff15a8d7eaf067c235b61dea23ac5e53
Marcel Sawicki <marcel.sawicki@gmail.com>
2022-01-18 12:24:20 +0100

    Update

---
+++ CSharp/Domain-Driven-Design/SnackMachine/SnackMachine/TestProject2/UnitTest1.cs
@@ -0,0 +1,26 @@
+using FluentAssertions;
+using SnackMachineApp.Logic;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace TestProject2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var snackMachine = new SnackMachine();
+            snackMachine.LoadSnacks(1, new Snack("Some snack"), 10, 1m);
+            snackMachine.InsertMoney(Money.Dollar);
+
+            snackMachine.BuySnack(1);
+
+            snackMachine.MoneyInTransaction.Should().Be(Money.None);
+            snackMachine.MoneyInside.Amount.Should().Be(2m);
+            snackMachine.Slots.Single(x => x.Position == 1).Quantity.Should().Be(9);
+
+        }
+    }
+}
