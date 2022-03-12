ebbaf0e8fc6a24ec0209c1af55f547a426b6039b
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-03-12 13:23:03 +0100

    gate and shot

--- TypeScript/penalty-shooter/src/Draw.ts
+++ TypeScript/penalty-shooter/src/Draw.ts
@@ -12,6 +12,23 @@ public static DrawField(context:CanvasRenderingContext2D, canvas: HTMLCanvasElem
         context.fillStyle = "green";
         context.fillRect(0, 0, canvas.width, canvas.height);
     }
+public static DrawCube(context:CanvasRenderingContext2D, fi: number, teta: number, skala: number)
+{
+    let a = Draw.CountX(10,10,0,fi,teta,skala);
+    let b =  Draw.CountY(10,10,0,fi,teta,skala);
+    Draw.DrawLine(context, Draw.CountX(10,10,0,fi,teta,skala), Draw.CountY(10,10,0,fi,teta,skala), Draw.CountX(10,20,0,fi,teta,skala), Draw.CountY(10,20,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,10,0,fi,teta,skala), Draw.CountY(20,10,0,fi,teta,skala), Draw.CountX(20,20,0,fi,teta,skala), Draw.CountY(20,20,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,20,0,fi,teta,skala), Draw.CountY(20,20,0,fi,teta,skala), Draw.CountX(10,20,0,fi,teta,skala), Draw.CountY(10,20,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,10,0,fi,teta,skala), Draw.CountY(20,10,0,fi,teta,skala), Draw.CountX(10,10,0,fi,teta,skala), Draw.CountY(10,10,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,20,0,fi,teta,skala), Draw.CountY(20,20,0,fi,teta,skala), Draw.CountX(20,20,10,fi,teta,skala),   Draw.CountY(20,20,10,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,20,10,fi,teta,skala), Draw.CountY(20,20,10,fi,teta,skala), Draw.CountX(20,10,10,fi,teta,skala), Draw.CountY(20,10,10,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,10,10,fi,teta,skala), Draw.CountY(20,10,10,fi,teta,skala), Draw.CountX(20,10,0,fi,teta,skala), Draw.CountY(20,10,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(20,10,10,fi,teta,skala), Draw.CountY(20,10,10,fi,teta,skala), Draw.CountX(10,10,10,fi,teta,skala), Draw.CountY(10,10,10,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(10,10,10,fi,teta,skala), Draw.CountY(10,10,10,fi,teta,skala), Draw.CountX(10,10,0,fi,teta,skala), Draw.CountY(10,10,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(10,10,10,fi,teta,skala), Draw.CountY(10,10,10,fi,teta,skala), Draw.CountX(10,20,10,fi,teta,skala), Draw.CountY(10,20,10,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(10,20,10,fi,teta,skala), Draw.CountY(10,20,10,fi,teta,skala), Draw.CountX(10,20,0,fi,teta,skala), Draw.CountY(10,20,0,fi,teta,skala),'yellow');
+    Draw.DrawLine(context, Draw.CountX(0,20,10,fi,teta,skala), Draw.CountY(0,20,10,fi,teta,skala), Draw.CountX(30,20,10,fi,teta,skala), Draw.CountY(30,20,10,fi,teta,skala),'white');
+}
 
 public static DrawLine(context:CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number, color:string): void {
 	context.beginPath()
@@ -20,6 +37,37 @@ public static DrawLine(context:CanvasRenderingContext2D, x1: number, y1: number,
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
+}
+
+public static CountX(xa: number, ya: number, za: number, fi: number, teta: number, skala: number): number
+{
+	let wsp_x: number = 640;//160;
+	let wsp_y: number = 480;//120;
+	let dist: number =100;
+	
+	let xn: number = -Math.sin(fi*Math.PI/180)*xa+Math.cos(fi*Math.PI/180)*ya;
+	let yn: number = -Math.cos(fi*Math.PI/180)*xa-Math.sin(fi*Math.PI/180)*ya+Math.sin(teta*Math.PI/180)*za;
+	let zn: number = -Math.cos(fi*Math.PI/180)*Math.sin(teta*Math.PI/180)*ya-Math.cos(teta*Math.PI/180)*za+dist;
+	let x: number = skala*xn/(zn+dist)+wsp_x;
+    let y: number = skala*yn/(zn+dist)+wsp_y;
+	 
+	let xp: number = xa + za*(Math.cos((teta*Math.PI/180))*0.5);
+	return Math.floor(xp*skala);
+}
+
+public static CountY(xa: number, ya: number, za: number, fi: number, teta: number, skala: number): number
+{
+	let wsp_x: number = 1280;//160;
+	let wsp_y: number = 960;//120;
+	let dist: number = 100;
+	
+	let xn: number = -Math.sin(fi*Math.PI/180)*xa+Math.cos(fi*Math.PI/180)*ya;
+	let yn: number = -Math.cos(fi*Math.PI/180)*xa-Math.sin(fi*Math.PI/180)*ya+Math.sin(teta*Math.PI/180)*za;
+	let zn: number = -Math.cos(fi*Math.PI/180)*Math.sin(teta*Math.PI/180)*ya-Math.cos(teta*Math.PI/180)*za+dist;
+	let x: number = skala*xn/(zn+dist)+wsp_x;
+	let y: number = skala*yn/(zn+dist)+wsp_y;
+	let yp: number  = ya + za *(Math.sin((teta*Math.PI/180))*0.5);
+	return Math.floor(yp*skala);
 }
 
 }
--- TypeScript/penalty-shooter/src/Main.ts
+++ TypeScript/penalty-shooter/src/Main.ts
@@ -20,26 +20,39 @@ let context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderi
 
 Draw.DrawField(context, canvas);
 
+let fi: number = 60;
+let teta: number = 30;
+
+Draw.DrawCube(context,fi,teta, 30);
+
+
 window.onload = function()
 {
 	let x_p: number=0;
 	let y_p: number=0;
-	let alfa: number=75;
-	let v0: number=100;
+	let alfa: number=55;
+	let v0: number=90;
 	let deg = (Math.PI/180)
+	let di = 2000;
+	let xi = 450;
 	setInterval(function() {
-		Draw.DrawLine(context, x_p-8, y_p-8, x_p+8, y_p+8,'green');
+		// Draw.DrawLine(context, x_p-8, y_p-8, x_p+8, y_p+8,'green');
 		x_p++
+		xi = Math.floor(Math.tan(40*deg)*x_p);
 		y_p=Math.tan(alfa*deg)*x_p-(((9.81/(2*Math.pow(v0,2)*Math.pow(Math.cos(alfa*deg),2))))*Math.pow(x_p,2));
-		//y_p=x_p*Math.tan(45);
 		y_p=canvas.height - Math.floor(y_p);
-		Draw.DrawLine(context, x_p, y_p, x_p+3, y_p+3,'white');
+		Draw.DrawLine(context,Draw.CountX(xi,Math.floor(y_p),Math.floor(di-(1+x_p)),fi,teta,0.5),Draw.CountY(xi,Math.floor(y_p),Math.floor(di-(1+x_p)),fi,teta,0.5),Draw.CountX(xi,Math.floor(1+y_p),Math.floor(di-x_p),fi,teta,0.5),Draw.CountY(xi,Math.floor(1+y_p),Math.floor(di-x_p),fi,teta,0.5),'blue');
+
+		//y_p=x_p*Math.tan(45);
+		
+		
+		Draw.DrawLine(context, 100+x_p, y_p-650, 100+x_p+3, y_p+3-650,'gray');
 		if(y_p>canvas.height){
 			
 			showMessage(endMessage);
 			clearInterval(1);
 		}
-  }, 25);
+  }, 5);
 
   
 
