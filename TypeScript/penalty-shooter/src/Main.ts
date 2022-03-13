8e62fb0e425c0e89baa1e711920ca0760ff8bbc4
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-03-13 07:36:12 +0100

    movements

--- TypeScript/penalty-shooter/src/Main.ts
+++ TypeScript/penalty-shooter/src/Main.ts
@@ -20,7 +20,7 @@ let context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderi
 
 Draw.DrawField(context, canvas);
 
-let fi: number = 60;
+let fi: number = 90;
 let teta: number = 30;
 
 Draw.DrawCube(context,fi,teta, 30);
@@ -31,14 +31,16 @@ window.onload = function()
 	let x_p: number=0;
 	let y_p: number=0;
 	let alfa: number=55;
-	let v0: number=90;
+	let v0: number=120;  // ball beginning speed v0
 	let deg = (Math.PI/180)
-	let di = 2000;
+	let di = 2800;  //player position N-S)
+	let xip = 600; //player position W-E)
 	let xi = 450;
+	let player_deg = 5; //player kicks angle (degree)
 	setInterval(function() {
 		// Draw.DrawLine(context, x_p-8, y_p-8, x_p+8, y_p+8,'green');
 		x_p++
-		xi = Math.floor(Math.tan(40*deg)*x_p);
+		xi = xip+Math.floor(Math.tan(player_deg*deg)*x_p);
 		y_p=Math.tan(alfa*deg)*x_p-(((9.81/(2*Math.pow(v0,2)*Math.pow(Math.cos(alfa*deg),2))))*Math.pow(x_p,2));
 		y_p=canvas.height - Math.floor(y_p);
 		Draw.DrawLine(context,Draw.CountX(xi,Math.floor(y_p),Math.floor(di-(1+x_p)),fi,teta,0.5),Draw.CountY(xi,Math.floor(y_p),Math.floor(di-(1+x_p)),fi,teta,0.5),Draw.CountX(xi,Math.floor(1+y_p),Math.floor(di-x_p),fi,teta,0.5),Draw.CountY(xi,Math.floor(1+y_p),Math.floor(di-x_p),fi,teta,0.5),'blue');
