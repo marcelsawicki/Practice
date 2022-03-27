4f14bd1ea6a695a4e3c03924f32d5246b9374b4b
marcel.sawicki@gmail.com <marcel.sawicki@gmail.com>
2022-03-27 12:28:54 +0200

    Parametry

--- TypeScript/penalty-shooter/src/Main.ts
+++ TypeScript/penalty-shooter/src/Main.ts
@@ -8,12 +8,21 @@ messanger.ShowMessage("Penalty shooter");
 let canvas: HTMLCanvasElement = document.getElementById('kanwa') as HTMLCanvasElement;
 let context: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
 let shootButton: HTMLButtonElement = document.getElementById('shoot-button') as HTMLButtonElement;
+let rzut_ukosny_kat_input: HTMLButtonElement = document.getElementById('rzut_ukosny_kat') as HTMLButtonElement;
+let rzut_ukosny_predkosc_poczatkowa_input: HTMLButtonElement = document.getElementById('rzut_ukosny_predkosc_poczatkowa') as HTMLButtonElement;
+let kat_horizontal_input: HTMLButtonElement = document.getElementById('kat_horizontal') as HTMLButtonElement;
 
 let draw: Draw = new Draw();
 
 draw.DrawField(context, canvas).then(() => {
 	draw.DrawGate(context);
-	shootButton.addEventListener("click", ()=>{ draw.Shoot(context, messanger,40,100,5);});	
+
+	shootButton.addEventListener("click", (event)=>{ 
+		let rzut_ukosny_kat: number = Number.parseInt(rzut_ukosny_kat_input.value);
+		let rzut_ukosny_predkosc_poczatkowa: number = Number.parseInt(rzut_ukosny_predkosc_poczatkowa_input.value);
+		let kat_horizontal: number = Number.parseInt(kat_horizontal_input.value);
+		draw.Shoot(context, messanger, rzut_ukosny_kat, rzut_ukosny_predkosc_poczatkowa, kat_horizontal);
+	});	
 	//draw.Shoot(context, messanger,40,100,5);
 });
 
