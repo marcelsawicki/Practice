a5ab82c71b34d6e4ed92a96925ac6cc379f0ccfa
Marcel <marcel.sawicki@insert.com.pl>
2024-10-08 20:06:20 +0200

    Update

--- Angular/angular-signal-example-1/src/app/app.component.ts
+++ Angular/angular-signal-example-1/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,27 @@ export class AppComponent {
   quantity = signal(1);
   qtyAvailable = signal([1, 2, 3, 4, 5, 6]);
 
+  selectedProduct = signal<Product>({
+    id: 5,
+    name: 'Hammer',
+    price: 12
+  });
+
   constructor() {
     console.log('In constructor:', this.quantity());
+    effect(() => console.log('In effect', this.quantity()));
+    this.quantity.update(q => q * 2)
   }
 
   onQuantitySelected(qty: number) {
-    throw new Error('Method not implemented.');
-    }
+    this.quantity.set(qty);
+    // this.quantity.set(67);
+    // this.quantity.set(42);
+  }
+}
+
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
 }
